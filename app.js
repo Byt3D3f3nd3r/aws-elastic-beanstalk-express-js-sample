eeda05e4f1e89d43651a68e189961fe408520c7a
Byt3D3f3nd3r <chukwuzitereokoli@gmail.com>
2024-09-02 16:42:04 +0100

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Fuck The World!!!'));
+app.get('/', (req, res) => res.send('SEND THE ASTEROIDS !!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
